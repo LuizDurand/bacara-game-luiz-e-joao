af03f07d316522ab281480ba9be0e757c0c6253f
jmanoelinsper <joaompf@al.insper.edu.br>
2020-10-18 18:29:42 -0300

    adicionar carta se a soma for menor que 6

--- naipe.py
+++ naipe.py
@@ -5,23 +5,6 @@ naipe = 4
 cartas_por_naipe = 13
 cartas_por_baralho = naipe * cartas_por_naipe
 
-def carta_aleatoria ():
-    ndc = randint(1, cartas_por_baralho)
-
-    baralho = int(ndc / cartas_por_baralho)
-    naipe = int((ndc - cartas_por_baralho) / cartas_por_naipe)
-    numero = ndc 
-
-    while numero > cartas_por_naipe:
-        numero -= cartas_por_naipe
-
-    if numero >= 11:
-        numero = 0
-        
-    
-    return (ndc, numero)
-
-
 def verifica_carta(carta_verificar,lista_cartas):
     carta_unica=True
 
@@ -31,23 +14,24 @@ def verifica_carta(carta_verificar,lista_cartas):
                 carta_unica=False
     return carta_unica
 
+def carta_aleatoria (lista_cartas):
+    while True:
+        ndc = randint(1, cartas_por_baralho)
 
+        baralho = int(ndc / cartas_por_baralho)
+        naipe = int((ndc - cartas_por_baralho) / cartas_por_naipe)
+        numero = ndc 
 
+        while numero > cartas_por_naipe:
+            numero -= cartas_por_naipe
 
-def cartas_aleatorias (numeros_grupos):
-    lista_c = []
+        if numero >= 11:
+            numero = 0
+            
+        carta=(ndc, numero)
 
-    for i in range (0, numeros_grupos):
-        cartas_jogador = []
-
-        for j in range (0, 2):
-            while True:
-                carta_nova= carta_aleatoria()
-                if verifica_carta(carta_nova,lista_c):
-                    cartas_jogador.append(carta_nova)
-                    break
-        lista_c.append(cartas_jogador)
-    return lista_c
+        if verifica_carta(carta_nova,lista_c):
+            return carta
 
 
 def soma_cartas (cartas_jogador):
@@ -56,8 +40,30 @@ def soma_cartas (cartas_jogador):
     for carta in cartas_jogador:
         valor = carta[1]
         soma += carta
+    
+    while soma>=10:
+        soma=soma-10
+
+
     return soma 
+
+
+def cartas_aleatorias (numeros_grupos):
+    lista_c = []
+
+    for i in range (0, numeros_grupos):
+        cartas_jogador = []
+
+        cartas_jogador.append(carta_aleatoria(lista_cartas))
+        cartas_jogador.append(carta_aleatoria(lista_cartas))       
+
+        soma=soma_cartas(cartas_jogador)
+        if soma<6:
+            cartas_jogador.append(carta_aleatoria(lista_cartas))
         
+        lista_c.append(cartas_jogador)
+
+    return lista_c
 
 
     
