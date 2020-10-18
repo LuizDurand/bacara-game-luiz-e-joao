d7e1361dca07a312ee8fabd68fdcd00b317e4a7e
LuizDurand <72882241+LuizDurand@users.noreply.github.com>
2020-10-18 13:02:48 -0300

    .

--- naipe.py
+++ naipe.py
@@ -5,13 +5,26 @@ naipe = 4
 cartas_por_naipe = 13
 cartas_por_baralho = naipe * cartas_por_naipe
 
-def carta_aleatoria():
+def carta_aleatoria ():
     ndc = randint(1, cartas_por_baralho)
 
-    baralho = int(input(ndc / cartas_por_baralho))
-    naipe = int(input(ndc - cartas_por_baralho) / cartas_por_naipe)
+    baralho = int(ndc / cartas_por_baralho)
+    naipe = int((ndc - cartas_por_baralho) / cartas_por_naipe)
     numero = ndc - cartas_por_baralho
 
     while numero > 13:
         numero -= cartas_por_naipe
-        return numero
+    
+    return (ndc, numero)
+
+def cartas_aleatorias (numeros_grupos):
+    lista_c = []
+
+    for i in range (0, numeros_grupos):
+        lista_p = []
+
+        for j in range (0, 2):
+            while True: 
+
+
+
