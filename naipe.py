7dc70b37c513c6af9112fd9e31c9ad05d1a13703
LuizDurand <72882241+LuizDurand@users.noreply.github.com>
2020-10-18 12:00:20 -0300

    .

--- naipe.py
+++ naipe.py
@@ -4,20 +4,14 @@ import math
 naipe = 4
 cartas_por_naipe = 13
 cartas_por_baralho = naipe * cartas_por_naipe
-baralhos = int(input("Com quantos baralhos você quer jogar??"))
-cartas_totais = baralhos * cartas_por_baralho
 
 def carta_aleatoria():
-    i = randint(1, cartas_totais)
+    ndc = randint(1, cartas_por_baralho)
 
-    baralho = int(input(i / cartas_por_baralho))
-    baralho_indice = baralho * cartas_por_baralho
-    naipe = int(input(i - baralho_indice) / cartas_por_naipe)
-    numero = i - baralho_indice
+    baralho = int(input(ndc / cartas_por_baralho))
+    naipe = int(input(ndc - cartas_por_baralho) / cartas_por_naipe)
+    numero = ndc - cartas_por_baralho
 
     while numero > 13:
-
         numero -= cartas_por_naipe
-
-
-    return numero
+        return numero
